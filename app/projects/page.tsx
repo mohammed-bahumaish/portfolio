28cfe27345997e553ab97d754b1747c19b91ef3e
mohammed gehad <mohammed.gehad.1998@gmail.com>
2023-11-10 15:46:55 +0300

    refactor

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,11 @@
 import { allProjects } from "contentlayer/generated";
-import Link from "next/link";
 import { Card } from "../components/card";
 import { Navigation } from "../components/nav";
 import { Article } from "./article";
 
 export default async function ProjectsPage() {
-  const featured = allProjects[0];
+  const featured = allProjects.find(p=>p.title === 'Prisma Editor');
+  const restOfProjects = allProjects.filter(p=>p.title !== 'Prisma Editor');
   return (
     <div className="relative pb-16">
       <Navigation />
@@ -20,10 +20,9 @@ export default async function ProjectsPage() {
           <div className="col-span-1 md:col-span-3">
             <Card>
               <Article project={featured} />
-
             </Card>
           </div>
-          {allProjects.slice(1, allProjects.length)
+          {restOfProjects.slice(1, allProjects.length)
             .map((project) => (
               <div className="col-span-1 h-full" key={project.slug}>
                 <Card key={project.slug}>
